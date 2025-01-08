f49d2c67d5821fe66a766b25462f932bf2dd9a03
Erik Hasse <erik.g.hasse@gmail.com>
2025-01-07 20:56:13 -0600

    improve day 1 solution

--- src/day_1.clj
+++ src/day_1.clj
@@ -1,32 +1,40 @@
 (ns day-1
-  (:require [clojure.string :as str])
-  (:require [clojure.math :as math]))
+  (:require [clojure.math :as math]
+            [clojure.string :as str]))
 
-(def input (slurp "inputs/day1"))
+(def input
+  (->>
+    (slurp "inputs/day1")
+    (str/split-lines)
+    (map Integer/parseInt)))
+
 
 ; part 1
+(defn calc-fuel [x]
+  (- (math/floor-div x 3) 2))
 
-(defn calc-fuel [x] (- (math/floor-div x 3) 2))
-(->>
-  input
-  (str/split-lines)
-  (map read-string)
-  (map calc-fuel)
-  (reduce +))
+(defn total-fuel [func nums]
+  (->>
+    nums
+    (map func)
+    (apply +)))
+
+
+(def part1
+  (partial total-fuel calc-fuel))
+
+(part1 input)
+
 
 ; part 2
 (defn calc-fuel-tyranny [x]
   (->>
     (iterate calc-fuel x)
-    (drop 1)
+    (rest)
     (take-while pos?)
-    (reduce +)
- ))
+    (apply +)))
 
-(->>
-  input
-  (str/split-lines)
-  (map read-string)
-  (map calc-fuel-tyranny)
-  (reduce +)
-  )
+(def part2
+  (partial total-fuel calc-fuel-tyranny))
+
+(part2 input)
